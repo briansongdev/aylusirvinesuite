02de2e9b6e8d45f47e30d18b7a523a59a911fa3e
Brian S <brians3476@gmail.com>
2022-06-13 17:36:51 -0700

    FINALLY FINISHED (solved env error)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+require("dotenv").config();
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    A1: process.env.A1,
+    A2: process.env.A2,
+    A3: process.env.A3,
+    A4: process.env.A4,
+    A5: process.env.A5,
+    A6: process.env.A6,
+  },
+};
