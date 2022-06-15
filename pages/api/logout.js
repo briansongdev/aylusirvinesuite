bcf0b4378f2e7f2f7e5268a6d63b4e4fd22b3874
Brian S <brians3476@gmail.com>
2022-06-15 16:09:48 -0700

    ok, DONE

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,7 +4,6 @@ function handler(req, res) {
     "user=deleted;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT"
   );
   res.setHeader("Location", "/");
-  res.status(302).end();
   res.redirect("/");
 }
 
